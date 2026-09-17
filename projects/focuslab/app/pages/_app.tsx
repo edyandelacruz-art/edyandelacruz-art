672414a810a1353f44d96c688adeea6de02e125c
edyandelacruz-art <edyan.delacruz@gmail.com>
2026-09-17 10:21:23 -0500

    FocusLab v0.7.1: load premium game card art styles

--- projects/focuslab/app/pages/_app.tsx
+++ projects/focuslab/app/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
 import '../styles/v071.css';
+import '../styles/v071-art.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
