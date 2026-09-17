952ab1d152d6a43b6af98e55b58876f5329e4703
edyandelacruz-art <edyan.delacruz@gmail.com>
2026-09-17 10:18:19 -0500

    FocusLab v0.7.1: use clean integration stylesheet

--- projects/focuslab/app/pages/_app.tsx
+++ projects/focuslab/app/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
-import '../styles/launchpad-v071.css';
+import '../styles/v071.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
